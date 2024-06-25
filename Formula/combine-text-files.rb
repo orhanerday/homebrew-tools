a21deecb56111aa9bc688019f4db1d50b46dc110
Orhan erday <orhanerday@gmail.com>
2024-06-25 16:55:54 +0300

    Update combine-text-files.rb

--- Formula/combine-text-files.rb
+++ Formula/combine-text-files.rb
@@ -7,7 +7,8 @@ class CombineTextFiles < Formula
 
   def install
     system "make"
-    bin.install "combine_text_files"
+    bin.install "combine-text-files"
+    bin.install_symlink "combine_text_files" => "combine-text-files"
   end
 
   test do
