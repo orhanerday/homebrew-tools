82eea3c8d2a44de55c7d44d7e27a6eb623c8bff3
Orhan erday <orhanerday@gmail.com>
2024-06-25 17:03:42 +0300

    Update combine-text-files.rb

--- Formula/combine-text-files.rb
+++ Formula/combine-text-files.rb
@@ -1,8 +1,8 @@
 class CombineTextFiles < Formula
   desc "A tool to combine text files with delimiters"
   homepage "https://github.com/orhanerday/combine-text-files"
-  url "https://codeload.github.com/orhanerday/combine-text-files/tar.gz/refs/tags/v2.1"
-  sha256 "0847300cb194eaaf04a1e5fa6522e5c506c72489044a8efd626589a19ddbc72a"
+  url "https://codeload.github.com/orhanerday/combine-text-files/tar.gz/refs/tags/v2.2"
+  sha256 "05ba5af21208caa5ac965e6c2e1495d55462f92f60c8ac79143d2ab19671e379"
   license "MIT"
 
   def install
@@ -12,7 +12,7 @@ class CombineTextFiles < Formula
 
   test do
     (testpath/"testfile.txt").write("This is a test file.")
-    system "#{bin}combine_text_files"
+    system "#{bin}combine-text-files"
     assert_predicate testpath/"single_file.txt", :exist?
   end
 end
